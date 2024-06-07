da06ae0dbc48ed7a42c5d4c31b69c754c0dd6369
Colby Fayock <fayock@gmail.com>
2024-06-07 13:49:54 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-50">
+      <div className="shadow rounded-lg bg-white p-12 py-11">
+        <h1 className="text-4xl font-bold text-zinc-800 mb-1">Space Jelly Palooza</h1>
+        <p className="text-xl mb-6">
+          The biggest party in the universe!
+        </p>
+        <h2 className="text-sm font-semibold">
+          When
+        </h2>
+        <ul className="mb-6">
+          <li>July 14, 2024</li>
+          <li>3:00pm to 11:59pm</li>
+        </ul>
+        <p>
+          Make sure to add it to your calendar!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
